41784a0d0acbe1a5c2d28f7e76d170cc883eba2e
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-04-07 15:33:35 -0700

    Pagination DOM tweak to select card. Update css for pagination and bootstrap card

--- static/pagination.js
+++ static/pagination.js
@@ -1,6 +1,6 @@
 const paginationNumbers = document.getElementById('pagination-numbers');
 const paginatedList = document.getElementById('paginated-list');
-const listItems = paginatedList.querySelectorAll('li');
+const listItems = Array.from(paginatedList.querySelectorAll('.park-card'));
 const nextButton = document.getElementById('next-button');
 const prevButton = document.getElementById('prev-button');
 
